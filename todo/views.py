f7f5ef9af827ae624d9585fb46aeb7919b8deb87
AnneKammenhed <anne.juriado@gmail.com>
2022-08-14 09:23:48 +0000

    add better todolut

--- todo/views.py
+++ todo/views.py
@@ -9,3 +9,12 @@ def get_todo_list(request):
         'items': items
     }
     return render(request, 'todo/todo_list.html', context)
+
+# Complete this function:
+def add_item(request):
+    if request.method == "POST":
+        name =
+        done =
+        Item.objects.create()
+        
+    return render(request, 'todo/add_item.html')
