f412b761764da3b361d4abf92e5664ef273dbadb
AnneKammenhed <anne.juriado@gmail.com>
2022-08-17 11:32:24 +0000

    remove 'HEROKU_HOSTNAME' from allowed hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -38,7 +38,7 @@ if development:
     ALLOWED_HOSTS = ['localhost']
 else:
     ALLOWED_HOSTS = [
-        os.environ.get('HEROKU_HOSTNAME', 'annes-djago-app.herokuapp.com')
+        os.environ.get('annes-djago-app.herokuapp.com')
     ]
 
 
