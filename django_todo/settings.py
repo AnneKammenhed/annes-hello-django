6af9da9668d25793c63269209b84b7fc145fd517
AnneKammenhed <anne.juriado@gmail.com>
2022-08-17 11:31:40 +0000

    add annes-djago-app.herokuapp.com to allowed-hosts

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -37,7 +37,9 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = [
+        os.environ.get('HEROKU_HOSTNAME', 'annes-djago-app.herokuapp.com')
+    ]
 
 
 # Application definition
